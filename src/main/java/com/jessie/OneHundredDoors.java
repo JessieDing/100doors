8169ca47c5c8fcc6404e4fd41862b2b48b98d495
Jessie Ding <nathalie.ding@gmail.com>
2016-10-25 17:03:40 +0800

    Refactor

--- src/main/java/com/jessie/OneHundredDoors.java
+++ src/main/java/com/jessie/OneHundredDoors.java
@@ -11,17 +11,26 @@ public class OneHundredDoors {
 
     public static void main(String[] args) {
         boolean[] doors = new boolean[100]; //用Boolean[] doors = new Boolean[100] 报错，空指针异常
-        int i;
-        int j;
-        for (i = 1; i <= doors.length; i++) {
-            for (j = 0; j < doors.length; j++) {
+        for (int i = 1; i <= doors.length; i++) {
+            for (int j = 0; j < doors.length; j++) {
                 if ((j + 1) % i == 0) {
                     doors[j] = !doors[j];
                 }
             }
         }
+
+        String[] doorStatus = new String[100];
+        for (int i = 0; i < doorStatus.length; i++) {
+            doorStatus[i] = String.valueOf(doors[i]);
+            if (doors[i]) {
+                doorStatus[i] = "open";
+            } else {
+                doorStatus[i] = "closed";
+            }
+        }
+
         for (int counter = 0; counter < 100; counter++) {
-            System.out.println("Door" + (counter + 1) + " is" + " " + doors[counter]);
+            System.out.println("Door" + (counter + 1) + " is" + " " + doorStatus[counter]);
         }
     }
 }
