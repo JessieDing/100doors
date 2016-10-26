2160f2152e49491043b46d8923aca5f8c9252d9c
Jessie Ding <nathalie.ding@gmail.com>
2016-10-26 22:37:45 +0800

    add notes

--- src/main/java/com/jessie/OneHundredDoors_old.java
+++ src/main/java/com/jessie/OneHundredDoors_old.java
@@ -10,7 +10,12 @@ public class OneHundredDoors_old {
 //    }
 
     public static void main(String[] args) {
-        boolean[] doors = new boolean[100]; //用Boolean[] doors = new Boolean[100] 报错，空指针异常
+        boolean[] doors = new boolean[100];
+        /*
+        用Boolean[] doors = new Boolean[100] 报错，空指针异常。
+        因为只是把数组这个对象new出来了，里面单个元素（对象）并没有被new出来。
+        没有new出来的对象都为null。
+        */
         for (int i = 1; i <= doors.length; i++) {
             for (int j = 0; j < doors.length; j++) {
                 if ((j + 1) % i == 0) {
