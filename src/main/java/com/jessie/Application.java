67a50f3240b3a027365f6c370d9cccfd0d7fe063
Jessie Ding <nathalie.ding@gmail.com>
2016-10-27 22:10:01 +0800

    Refactor, add DoorDao

--- src/main/java/com/jessie/Application.java
+++ src/main/java/com/jessie/Application.java
@@ -1,14 +1,19 @@
 package com.jessie;
 
+
 public class Application {
+
     public static void main(String[] args) {
         DoorTrigger doorTrigger = new DoorTrigger();
         Printer printer = new Printer();
+        DoorDao test = new DoorDao();
 
         Door[] doors = doorTrigger.trigger();
 
-        for (int count = 0; count < 100; count++){   // 用Door类的 getNumber，foreach遍历。更好
+        for (int count = 0; count < 100; count++) {   // 用Door类的 getNumber，foreach遍历。更好
             printer.print("Door" + (count + 1) + " is" + " " + doors[count].getStatus());
+            test.insert(count + 1, doors[count].getStatus());
         }
+        test.close();
     }
 }
---
+++ src/main/java/com/jessie/DoorDao.java
@@ -0,0 +1,46 @@
+package com.jessie;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DoorDao {
+    private final static String URL = "jdbc:mysql://localhost:3306/100doors";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "jieding";
+    private final static String DRIVER = "com.mysql.jdbc.Driver";
+    private Connection connection;
+
+    public DoorDao() {
+        try {             //???
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insert(int id, String status) {
+        try {
+            String sql = String.format("insert into game_result (id,status) values(%d,'%s')", id, status); //format的用法
+//            String sql = "insert into game_result (id,status) values(1,'closed')";
+            Statement state = connection.createStatement();
+            state.executeUpdate(sql);
+            state.close();
+            System.out.println("insert success");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
